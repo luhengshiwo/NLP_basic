3abcd96202572e870f41f85c468b3aa18a372806
luhengshiwo <luhengok@163.com>
2018-06-29 11:45:13 +0800

    delete data

--- silicon/multi_txt_classification/train_dev_test_split.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-sort -R data/cut.csv > data/sort.csv
-# 512 1024 2048 3072 4096 5120 6144 7168 7478
-head -n 76215 data/sort.csv > data/train.csv
-tail -n 2048 data/sort.csv > data/tmp.csv
-head -n 1024 data/tmp.csv > data/dev.csv
-tail -n 1024 data/tmp.csv > data/test.csv
-rm data/tmp.csv
-rm data/sort.csv
--- silicon/seq2right/train_dev_test_split.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-sort -R data/cut.csv > data/sort.csv
-head -n 9216 data/sort.csv > data/train.csv
-tail -n 1024 data/sort.csv > data/tmp.csv
-head -n 512 data/tmp.csv > data/dev.csv
-tail -n 512 data/tmp.csv > data/test.csv
-rm data/tmp.csv
-rm data/sort.csv
